8ebb9f7bce956b896fa375157b19d6c166793ca3
FERNANDO GUALBERTO BARBOZA JUNIOR <20252930002@E293-ACAD.LOCAL>
2026-09-24 19:43:19 -0300

    Add Projeto Conexao Banco

---
+++ ProjetoConexaoBanco/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ProjetoConexaoBanco.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
